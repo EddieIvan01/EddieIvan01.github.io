a10316cec0e5060b690c1754d4f633db522c47b2
EddieIvan01 <406917347@qq.com>
2019-01-01 00:19:43 +0800

    2018

--- assets/img/posts/新建文本文档.bat
+++ assets/img/posts/新建文本文档.bat
@@ -1,4 +1,4 @@
-set a=gin
+set a=2018
 copy C:\Users\40691\Desktop\blogimg\%a%.jpg C:\Users\40691\Desktop\blogimg\%a%_md.jpg
 copy C:\Users\40691\Desktop\blogimg\%a%.jpg C:\Users\40691\Desktop\blogimg\%a%_lg.jpg
 copy C:\Users\40691\Desktop\blogimg\%a%.jpg C:\Users\40691\Desktop\blogimg\%a%_placehold.jpg
